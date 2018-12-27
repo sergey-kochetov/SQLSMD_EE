6974e2ae983b35e50f7a4715f0750f2777b23885
sergey-kochetov <git.kochetov@gmail.com>
2018-12-27 20:18:26 +0300

    Крутим Spring  Data -> рефактор 2

--- src/main/java/com/juja/model/entity/UserActions.java
+++ src/main/java/com/juja/model/entity/UserActions.java
@@ -14,6 +14,8 @@ public class UserActions {
 
     @Column(name = "db_name")
     private String dbName;
+//    @Column(name = "database_id")
+//    private Database database;
 
     @Column(name = "action")
     private String action;
